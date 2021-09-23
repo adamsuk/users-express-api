a0a35120e5cb6b6e83d7de2d438b28f6478e68f1
Scott <sra405@hotmail.co.uk>
2021-09-23 22:51:02 +0100

    fix(gcp auth): setting auth env var failed heroku build

--- src/auth/config.js
+++ src/auth/config.js
@@ -1,8 +1,8 @@
 const admin = require('firebase-admin');
-const { GOOGLE_CREDENTIALS, FIREBASE_DB_URL } = require('../infra/envs');
+const { GOOGLE_APPLICATION_CREDENTIALS, FIREBASE_DB_URL } = require('../infra/envs');
 
 admin.initializeApp({
-  credential: admin.credential.cert(GOOGLE_CREDENTIALS),
+  credential: admin.credential.cert(GOOGLE_APPLICATION_CREDENTIALS),
   databaseURL: FIREBASE_DB_URL
 });
 
